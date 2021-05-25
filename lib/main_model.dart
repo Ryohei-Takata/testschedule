7d742236088754cacc7b3134179ba21f713dbeea
Ryohei-Takata <ryo-takata@sc-ko.jp>
2021-05-25 23:35:25 +0900

    providerパターンでモデルを作ってみる

---
+++ lib/main_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MainModel extends ChangeNotifier {
+  String tboyText = 'Tboyさんじゃないよ';
+
+  void changeTboyText() {
+    tboyText = 'tboyさんかっこいい';
+    notifyListeners();
+  }
+}
